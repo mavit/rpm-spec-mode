c34dfd343dcc69b53c8200321659f3fe8a98c377
Björn Bidar <bjorn.bidar@thaodan.de>
2024-12-07 02:46:37 +0200

    Improve package metadata replaces references to XEmacs

--- rpm-spec-mode.el
+++ rpm-spec-mode.el
@@ -3,23 +3,32 @@
 ;; Copyright (C) 1997-2015 Stig Bjørlykke, <stig@bjorlykke.org>
 
 ;; Author:   Stig Bjørlykke, <stig@bjorlykke.org>
-;; Keywords: unix, languages
+;;	Tore Olsen <toreo@tihlde.org>
+;;	Steve Sanbeg <sanbeg@dset.com>
+;;	Tim Powers <timp@redhat.com>
+;;	Trond Eivind Glomsrød <teg@redhat.com>
+;;	Chmouel Boudjnah <chmouel@mandrakesoft.com>
+;;	Ville Skyttä <ville.skytta@iki.fi>
+;;	Adam Spiers <elisp@adamspiers.org>
+
+;; Keywords: unix languages rpm
 ;; Version:  0.16
+;; URL: https://github.com/Thaodan/rpm-spec-mode/
+;; Package-Requires: ((emacs "27.1"))
 
-;; This file is part of XEmacs.
-
-;; XEmacs is free software; you can redistribute it and/or modify
+;; This file was part of XEmacs.
+;; RPM-Spec-Mode is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; XEmacs is distributed in the hope that it will be useful,
+;; RPM-Spec-Mode is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with XEmacs; see the file COPYING.  If not, write to the
+;; along with RPM-Spec-Mode; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301 USA.
 
