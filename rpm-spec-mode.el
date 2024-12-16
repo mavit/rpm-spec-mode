76a9e28501f6e6802f7825b4301c7e8ce079aced
Peter Oliver <git@mavit.org.uk>
2024-12-16 12:42:01 +0000

    Drop outdated link to old Git repository

--- rpm-spec-mode.el
+++ rpm-spec-mode.el
@@ -61,9 +61,6 @@
 
 ;; This mode is used for editing spec files used for building RPM packages.
 ;;
-;; Most recent version is available from:
-;;  <https://github.com/bjorlykke/rpm-spec-mode>
-;;
 ;; Put this in your .emacs file to enable autoloading of rpm-spec-mode,
 ;; and auto-recognition of ".spec" files:
 ;;
