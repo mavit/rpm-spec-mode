08a0e10fd965e21eec5cb83d6387293f47d75b97
Peter Oliver <git@mavit.org.uk>
2024-12-16 12:35:08 +0000

    Require at least Emacs 29, which provided new macro defvar-keymap

--- rpm-spec-mode.el
+++ rpm-spec-mode.el
@@ -15,7 +15,7 @@
 ;; Keywords: unix languages rpm
 ;; Version:  0.16
 ;; URL: https://github.com/Thaodan/rpm-spec-mode/
-;; Package-Requires: ((emacs "27.1"))
+;; Package-Requires: ((emacs "29.0.90"))
 
 ;; This file was part of XEmacs.
 ;; RPM-Spec-Mode is free software; you can redistribute it and/or modify
